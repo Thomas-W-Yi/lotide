37acddfdc603526ca532bf1d6421831906a8f44d
Your Name <thomas.w.yee@gmail.com>
2021-01-04 19:06:37 -0500

    reverse

---
+++ reverse.js
@@ -0,0 +1,17 @@
+const reverse = (args) => {
+  let arrayOfArgs = [...args];
+
+  arrayOfArgs.map((x) => {
+    let subArr = [],
+      len = x.length,
+      subStr;
+    for (let i = 0; i < len; i++) {
+      subArr.push(x[len - i - 1]);
+    }
+    subStr = subArr.join('');
+    console.log(subStr);
+  });
+};
+let a, b, rest;
+[a, b, ...rest] = process.argv;
+reverse(rest);
