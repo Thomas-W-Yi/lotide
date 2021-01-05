8e908e8b796f9174dffcdfe92bf7648fe906d032
Your Name <thomas.w.yee@gmail.com>
2021-01-05 17:55:56 -0500

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  let fail = '🛑',
+    pass = '✅';
+  console.log(
+    actual === expected
+      ? `${pass}${pass}${pass}Assertion Passed: ${actual} === ${expected}`
+      : `${fail}${fail}${fail}Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+const eqArrays = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(['1', '2', '3'], ['1', '2', '3'])); // => true
+console.log(eqArrays(['1', '2', '3'], ['1', '2', 3])); // => f
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
