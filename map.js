b1acda906918d68350cf056d7f42b9dbcafbfddd
Your Name <thomas.w.yee@gmail.com>
2021-01-07 14:17:38 -0500

    add code testing

--- map.js
+++ map.js
@@ -8,3 +8,27 @@ const map = (array, callback) => {
 };
 const results1 = map(words, (word) => word[0]);
 console.log(results1);
+
+// test code
+const eqArrays = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (a1, a2) => {
+  let fail = '🛑',
+    pass = '✅';
+  return eqArrays(a1, a2)
+    ? `${pass}${pass}${pass}Assertion Passed: [${a1}] === [${a2}]`
+    : `${fail}${fail}${fail}Assertion Failed: [${a1}] !== [${a2}]`;
+};
+console.log(
+  assertArraysEqual(
+    map(words, (word) => word[0]),
+    ['g', 'c', 't', 'm', 't']
+  )
+);
