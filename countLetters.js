ad57290ce7dfef22a9d4a9df516f5c8b7fa566d3
Your Name <thomas.w.yee@gmail.com>
2021-01-06 19:32:35 -0500

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+  let fail = '🛑',
+    pass = '✅';
+  console.log(
+    actual === expected
+      ? `${pass}${pass}${pass}Assertion Passed: ${actual} === ${expected}`
+      : `${fail}${fail}${fail}Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+
+const countLetters = (string) => {
+  let result = {};
+  for (let letter of string) {
+    result[letter] ? result[letter]++ : (result[letter] = 1);
+  }
+  return result;
+};
+
+assertEqual(countLetters('LHL')['L'], 2);
