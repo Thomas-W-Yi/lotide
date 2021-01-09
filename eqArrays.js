52d4f869cea9ba05cf2fc2c922cb00c4a0b965ad
Your Name <thomas.w.yee@gmail.com>
2021-01-08 22:07:13 -0500

    eqArrays recursive

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+const log = console.log;
 const assertEqual = function (actual, expected) {
   let fail = '🛑',
     pass = '✅';
@@ -10,17 +11,20 @@ const assertEqual = function (actual, expected) {
 const eqArrays = (array1, array2) => {
   let len = array1.length > array2.length ? array1.length : array2.length;
   for (let i = 0; i < len; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (eqArrays(array1[i], array2[i]) === false) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(['1', '2', '3'], ['1', '2', '3'])); // => true
-console.log(eqArrays(['1', '2', '3'], ['1', '2', 3])); // => f
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+log(eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+// => true
+log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+// => false
+log(eqArrays([[2, 3], [4]], [[2, 3], 4]));
+// => false
