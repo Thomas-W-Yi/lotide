9365a03c08af9d4df141d0de4050d406bef6ccfa
Your Name <thomas.w.yee@gmail.com>
2021-01-11 13:51:35 -0500

    middle refactor

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const eqArrays = (array1, array2) => {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (a1, a2) => {
-  let fail = '🛑',
-    pass = '✅';
-  return eqArrays(a1, a2)
-    ? `${pass}${pass}${pass}Assertion Passed: ${a1} === ${a2}`
-    : `${fail}${fail}${fail}Assertion Failed: ${a1} !== ${a2}`;
-};
-
 const middle = (array) => {
   if (array.length <= 2) {
     return [];
@@ -24,5 +7,5 @@ const middle = (array) => {
       : [array[(array.length - 1) / 2]];
   }
 };
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+
+module.exports = middle;
