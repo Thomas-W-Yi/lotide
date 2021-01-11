1b13a2e228672ce154d99297213d2b204bec4e7f
Your Name <thomas.w.yee@gmail.com>
2021-01-11 14:20:54 -0500

    unit test for tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,8 @@
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-//test code
-console.log(eqArrays(tail([0, 1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]));
-
+describe('#tail', () => {
+  it('return [1,2,3,4,5] for [0,1,2,3,4,5]', () => {
+    assert.deepEqual(tail([0, 1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+  });
+});
